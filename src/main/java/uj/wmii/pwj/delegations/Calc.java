b8f1bd6a92589ba4ac7828e144c0f2fd2a453730
Arek Sokolowski <arek.sokolowski@gmail.com>
2023-09-29 20:33:36 +0200

    first commit

---
+++ src/main/java/uj/wmii/pwj/delegations/Calc.java
@@ -0,0 +1,9 @@
+package uj.wmii.pwj.delegations;
+
+import java.math.BigDecimal;
+
+public class Calc {
+    BigDecimal calculate(String name, String start, String end, BigDecimal dailyRate) {
+        return null;
+    }
+}
