684bad7cf9ca54d8128b77f80472b82be1408751
Miroslav Dovger <miroslavdovger@gmail.com>
2024-01-07 18:41:26 +0100

    project

--- src/main/java/uj/wmii/pwj/delegations/Calc.java
+++ src/main/java/uj/wmii/pwj/delegations/Calc.java
@@ -1,9 +1,30 @@
 package uj.wmii.pwj.delegations;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Calc {
     BigDecimal calculate(String name, String start, String end, BigDecimal dailyRate) {
-        return null;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm VV");
+        Duration difference = Duration.between(
+                ZonedDateTime.parse(start, dtf),
+                ZonedDateTime.parse(end, dtf)
+        );
+
+        long days = difference.toDays();
+        long hours = difference.minusDays(days).toHours();
+        long minutes = difference.minusDays(days).minusHours(hours).toMinutes();
+
+        BigDecimal salary = dailyRate.multiply(BigDecimal.valueOf(days));
+
+        return salary.add(
+                (hours > 12) ? dailyRate :
+                        (hours > 8) ? dailyRate.divide(BigDecimal.valueOf(2), RoundingMode.HALF_UP) :
+                                (hours > 0 || minutes > 0) ? dailyRate.divide(BigDecimal.valueOf(3), RoundingMode.HALF_UP) :
+                                        BigDecimal.ZERO
+        );
     }
 }
